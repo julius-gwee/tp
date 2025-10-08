7f32b9a918f4921e6c0895373e55ed2804b1fec5
Julius Gwee <e1406504@u.nus.edu>
2025-10-09 04:45:04 +0800

    Extend JSON Adapter for tags
    Let's
    * Make adapter carry category, colour and description metadata
    * Validate each field

--- src/main/java/seedu/address/storage/JsonAdaptedTag.java
+++ src/main/java/seedu/address/storage/JsonAdaptedTag.java
@@ -1,7 +1,7 @@
 package seedu.address.storage;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonValue;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 import seedu.address.commons.exceptions.IllegalValueException;
 import seedu.address.model.tag.Tag;
@@ -11,14 +11,29 @@ import seedu.address.model.tag.Tag;
  */
 class JsonAdaptedTag {
 
+    public static final String MISSING_FIELD_MESSAGE_FORMAT = "Tag's %s field is missing!";
+
     private final String tagName;
+    private final String category;
+    private final String colour;
+    private final String description;
 
     /**
      * Constructs a {@code JsonAdaptedTag} with the given {@code tagName}.
      */
     @JsonCreator
-    public JsonAdaptedTag(String tagName) {
+    public JsonAdaptedTag(@JsonProperty("tagName") String tagName,
+                          @JsonProperty("category") String category,
+                          @JsonProperty("colour") String colour,
+                          @JsonProperty("description") String description) {
         this.tagName = tagName;
+        this.category = category;
+        this.colour = colour;
+        this.description = description;
+    }
+
+    public JsonAdaptedTag(String tagName) {
+        this(tagName, null, null, null);
     }
 
     /**
@@ -26,11 +41,29 @@ class JsonAdaptedTag {
      */
     public JsonAdaptedTag(Tag source) {
         tagName = source.tagName;
+        category = source.category;
+        colour = source.colour;
+        description = source.description;
     }
 
-    @JsonValue
+    @JsonProperty("tagName")
     public String getTagName() {
         return tagName;
+    }
+
+    @JsonProperty("category")
+    public String getCategory() {
+        return category;
+    }
+
+    @JsonProperty("colour")
+    public String getColour() {
+        return colour;
+    }
+
+    @JsonProperty("description")
+    public String getDescription() {
+        return description;
     }
 
     /**
@@ -39,10 +72,28 @@ class JsonAdaptedTag {
      * @throws IllegalValueException if there were any data constraints violated in the adapted tag.
      */
     public Tag toModelType() throws IllegalValueException {
+        if (tagName == null) {
+            throw new IllegalValueException(String.format(MISSING_FIELD_MESSAGE_FORMAT, "name"));
+        }
         if (!Tag.isValidTagName(tagName)) {
             throw new IllegalValueException(Tag.MESSAGE_CONSTRAINTS);
         }
-        return new Tag(tagName);
-    }
 
+        final String modelCategory = category == null ? Tag.DEFAULT_CATEGORY : category;
+        if (!Tag.isValidCategory(modelCategory)) {
+            throw new IllegalValueException(Tag.MESSAGE_CATEGORY_CONSTRAINTS);
+        }
+
+        final String modelColour = colour == null ? Tag.DEFAULT_COLOUR : colour;
+        if (!Tag.isValidColour(modelColour)) {
+            throw new IllegalValueException(Tag.MESSAGE_COLOUR_CONSTRAINTS);
+        }
+
+        final String modelDescription = description == null ? Tag.DEFAULT_DESCRIPTION : description;
+        if (!Tag.isValidDescription(modelDescription)) {
+            throw new IllegalValueException(Tag.MESSAGE_DESCRIPTION_CONSTRAINTS);
+        }
+
+        return new Tag(tagName, modelCategory, modelColour, modelDescription);
+    }
 }
