b57e42ec1b0e0963c6004eed4db69b2cb0c31fd4
isyraq <misyraq2003@gmail.com>
2025-10-16 00:03:32 +0800

    Update add command naming

--- src/test/java/seedu/address/logic/commands/AddCommandIntegrationTest.java
+++ src/test/java/seedu/address/logic/commands/AddCommandIntegrationTest.java
@@ -28,21 +28,21 @@ public class AddCommandIntegrationTest {
 
     @Test
     public void execute_newPerson_success() {
-        Person validPerson = new PersonBuilder().build();
+        Person validCandidate = new PersonBuilder().build();
 
         Model expectedModel = new ModelManager(model.getCandidateList(), new UserPrefs());
-        expectedModel.addCandidate(validPerson);
+        expectedModel.addCandidate(validCandidate);
 
-        assertCommandSuccess(new AddCommand(validPerson), model,
-                String.format(AddCommand.MESSAGE_SUCCESS, Messages.format(validPerson)),
+        assertCommandSuccess(new AddCommand(validCandidate), model,
+                String.format(AddCommand.MESSAGE_SUCCESS, Messages.format(validCandidate)),
                 expectedModel);
     }
 
     @Test
     public void execute_duplicatePerson_throwsCommandException() {
-        Person personInList = model.getCandidateList().getCandidateList().get(0);
-        assertCommandFailure(new AddCommand(personInList), model,
-                AddCommand.MESSAGE_DUPLICATE_PERSON);
+        Person candidateInList = model.getCandidateList().getCandidateList().get(0);
+        assertCommandFailure(new AddCommand(candidateInList), model,
+                AddCommand.MESSAGE_DUPLICATE_CANDIDATE);
     }
 
 }
