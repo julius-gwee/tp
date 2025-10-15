4c273350c68db8fe671f47b05367166d26c85f84
dominicddl <e1398293@u.nus.edu>
2025-10-15 12:28:44 +0800

    Create KanbanColumn component for kanban board layout

---
+++ src/main/java/seedu/address/ui/KanbanColumn.java
@@ -0,0 +1,54 @@
+package seedu.address.ui;
+
+import java.util.logging.Logger;
+
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
+import javafx.scene.layout.Region;
+import seedu.address.commons.core.LogsCenter;
+import seedu.address.model.person.Person;
+
+/**
+ * A kanban column that displays a list of persons with a header.
+ */
+public class KanbanColumn extends UiPart<Region> {
+    private static final String FXML = "KanbanColumn.fxml";
+    private final Logger logger = LogsCenter.getLogger(KanbanColumn.class);
+
+    @FXML
+    private Label columnHeader;
+
+    @FXML
+    private ListView<Person> personListView;
+
+    /**
+     * Creates a {@code KanbanColumn} with the given header and {@code ObservableList}.
+     */
+    public KanbanColumn(String headerText, ObservableList<Person> personList) {
+        super(FXML);
+        columnHeader.setText(headerText);
+        personListView.setItems(personList);
+        personListView.setCellFactory(listView -> new PersonListViewCell());
+    }
+
+    /**
+     * Custom {@code ListCell} that displays the graphics of a {@code Person} using a {@code PersonCard}.
+     */
+    class PersonListViewCell extends ListCell<Person> {
+        @Override
+        protected void updateItem(Person person, boolean empty) {
+            super.updateItem(person, empty);
+
+            if (empty || person == null) {
+                setGraphic(null);
+                setText(null);
+            } else {
+                setGraphic(new PersonCard(person, getIndex() + 1).getRoot());
+            }
+        }
+    }
+}
+
