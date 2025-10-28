74986c5ce022298ec84fbb6287280ff8f93a100e
qiiiixuan <leleqx123@gmail.com>
2025-10-28 16:20:06 +0800

    Resolve checkstyle error

--- src/test/java/seedu/address/logic/commands/SortCommandTest.java
+++ src/test/java/seedu/address/logic/commands/SortCommandTest.java
@@ -4,8 +4,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
-import static seedu.address.logic.commands.SortCommand.SORT_BY_DATEADDED;
 import static seedu.address.logic.commands.SortCommand.SORT_BY_ALPHABET;
+import static seedu.address.logic.commands.SortCommand.SORT_BY_DATEADDED;
 import static seedu.address.testutil.TypicalPersons.getTypicalFindr;
 
 import java.util.Comparator;
