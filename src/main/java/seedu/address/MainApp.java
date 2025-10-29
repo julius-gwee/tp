1765fb4fbb71fa1e9072e40b9afd9cc9b79e3f39
dominicddl <e1398293@u.nus.edu>
2025-10-29 15:42:23 +0800

    Change terminal output header

--- src/main/java/seedu/address/MainApp.java
+++ src/main/java/seedu/address/MainApp.java
@@ -50,7 +50,7 @@ public class MainApp extends Application {
 
     @Override
     public void init() throws Exception {
-        logger.info("=============================[ Initializing AddressBook ]===========================");
+        logger.info("=============================[ Initializing Findr ]===========================");
         super.init();
 
         AppParameters appParameters = AppParameters.parse(getParameters());
@@ -71,9 +71,9 @@ public class MainApp extends Application {
     }
 
     /**
-     * Returns a {@code ModelManager} with the data from {@code storage}'s address book and {@code userPrefs}. <br>
-     * The data from the sample address book will be used instead if {@code storage}'s address book is not found,
-     * or an empty address book will be used instead if errors occur when reading {@code storage}'s address book.
+     * Returns a {@code ModelManager} with the data from {@code storage}'s candidate list and {@code userPrefs}. <br>
+     * The data from the sample candidate list will be used instead if {@code storage}'s candidate list is not found,
+     * or an empty candidate list will be used instead if errors occur when reading {@code storage}'s candidate list.
      */
     Model initModelManager(Storage storage, ReadOnlyUserPrefs userPrefs) {
         logger.info("Using data file : " + storage.getFindrFilePath());
@@ -84,12 +84,12 @@ public class MainApp extends Application {
             addressBookOptional = storage.readCandidateList();
             if (!addressBookOptional.isPresent()) {
                 logger.info("Creating a new data file " + storage.getFindrFilePath()
-                        + " populated with a sample AddressBook.");
+                        + " populated with sample data.");
             }
             initialData = addressBookOptional.orElseGet(SampleDataUtil::getSampleAddressBook);
         } catch (DataLoadingException e) {
             logger.warning("Data file at " + storage.getFindrFilePath() + " could not be loaded."
-                    + " Will be starting with an empty AddressBook.");
+                    + " Will be starting with an empty candidate list.");
             initialData = new Findr();
         }
 
@@ -173,13 +173,13 @@ public class MainApp extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-        logger.info("Starting AddressBook " + MainApp.VERSION);
+        logger.info("Starting Findr " + MainApp.VERSION);
         ui.start(primaryStage);
     }
 
     @Override
     public void stop() {
-        logger.info("============================ [ Stopping AddressBook ] =============================");
+        logger.info("============================ [ Stopping Findr ] =============================");
         try {
             storage.saveUserPrefs(model.getUserPrefs());
         } catch (IOException e) {
