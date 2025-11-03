f5474562086e7c412ada35b150487a8e066c7b80
Julius Gwee <e1406504@u.nus.edu>
2025-11-03 15:54:23 +0800

    Update error message

--- src/main/java/seedu/address/model/person/Name.java
+++ src/main/java/seedu/address/model/person/Name.java
@@ -13,7 +13,7 @@ public class Name {
 
     public static final String MESSAGE_CONSTRAINTS =
             "Names should only contain letters (including accented characters), numbers, spaces, apostrophes, '@',"
-                    + " or hyphens, and it should not be blank";
+                    + " or hyphens, may include 's/o' or 'd/o', and it should not be blank";
 
     /*
      * The first character of the address must not be a whitespace,
